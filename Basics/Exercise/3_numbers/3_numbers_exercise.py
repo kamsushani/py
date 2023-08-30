d12463ea919c28a45cbde0b89344c75aacfd03f5
kamsushani <kamsushani@gmail.com>
2023-08-30 22:56:14 +0900

    first commit

--- Basics/Exercise/3_numbers/3_numbers_exercise.py
+++ Basics/Exercise/3_numbers/3_numbers_exercise.py
@@ -28,3 +28,7 @@ print("total cost for bathroom tiles replacement:",cost) # Ans: 15125.0
 # 4. Print binary representation of number 17
 num=17
 print('Binary of number 17 is:',format(num,'b')) # Ans: 10001
+
+# 5. Print binary representation of number 20
+num=20
+print('Binary of number 20 is:',format(num,'b')) # Ans: 10001
